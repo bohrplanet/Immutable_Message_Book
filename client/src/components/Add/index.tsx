707dc48db971577c67ee32bc0cba51e845a9f13b
Yinbo <bohrplanet@gmail.com>
2022-03-09 12:17:42 -0800

    1. improve animation effect

--- client/src/components/Add/index.tsx
+++ client/src/components/Add/index.tsx
@@ -1,9 +1,9 @@
 import React, { Component, TextareaHTMLAttributes } from "react";
-import { Divider, Button, Input } from 'antd';
+import { Divider, Button, Input, message } from 'antd';
 import './index.css'
 
 interface IProps {
-    addPrediction : Function
+    addPrediction: Function
 }
 
 interface IState {
@@ -13,20 +13,25 @@ interface IState {
 const { TextArea } = Input;
 
 export default class Add extends Component<IProps, IState> {
-// export default class Add extends Component {
+    // export default class Add extends Component {
 
     inputRef = React.createRef<any>();
     // inputRef = React.createRef(null);
 
     sendToApp = () => {
+
+        // TODO 如果输入为空，则禁止输入
+        if (this.inputRef.current?.resizableTextArea.textArea.value == "") {
+            return message.info('Message content is empty.');
+        }
         // const {inputRef} = this;
         // 将textarea中的内容传给APP处理
         // console.log("input value is ", this.inputRef.current?.resizableTextArea.textArea.value);
         // console.log("input value is ", this.inputRef.current?.value);
         this.props.addPrediction(this.inputRef.current?.resizableTextArea.textArea.value);
 
-        // TODO 如果输入为空，则禁止输入
         // TODO 上传消息后，输入栏清空
+        this.inputRef.current!.resizableTextArea.value = ""
     }
 
     render() {
@@ -34,7 +39,7 @@ export default class Add extends Component<IProps, IState> {
             <div className="todo-header">
                 <Divider />
                 {/* <p className="word">write a message</p> */}
-                <TextArea id="content" ref={this.inputRef} cols={10} rows={8} placeholder="Type your prediction"/>
+                <TextArea id="content" ref={this.inputRef} cols={10} rows={8} placeholder="Type your prediction" />
                 {/* <textarea id="content" ref={this.inputRef} onChange={change2} cols={50} rows={7}/> */}
                 <br />
                 {/* <button onClick={this.sendToApp}>Post My Prediction</button> */}
