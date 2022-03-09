43877ce1728d8a91d201fcbee404fd7836ac9b4d
Yinbo <bohrplanet@gmail.com>
2022-03-09 07:45:20 -0800

    1. change jsx file to tsx file.

--- client/src/components/Add/index.tsx
+++ client/src/components/Add/index.tsx
@@ -2,34 +2,35 @@ import React, { Component } from "react";
 import { Button, Input } from 'antd';
 import './index.css'
 
-// interface IProps {
-//     addPrediction : Function
-// }
+interface IProps {
+    addPrediction : Function
+}
 
-// interface IState {
+interface IState {
 
-// }
+}
 
 const { TextArea } = Input;
 
-// export default class Add extends Component<IProps, IState> {
-export default class Add extends Component {
+export default class Add extends Component<IProps, IState> {
+// export default class Add extends Component {
 
-    // inputRef = React.createRef<HTMLTextAreaElement>();
-    inputRef = React.createRef();
+    inputRef = React.createRef<any>();
+    // inputRef = React.createRef(null);
 
     sendToApp = () => {
         // const {inputRef} = this;
         // 将textarea中的内容传给APP处理
-        // console.log("input value is ", inputRef.current?.value);
-        this.props.addPrediction(this.inputRef.current?.value);
+        // console.log("input value is ", this.inputRef.current?.resizableTextArea.textArea.value);
+        // console.log("input value is ", this.inputRef.current?.value);
+        this.props.addPrediction(this.inputRef.current?.resizableTextArea.textArea.value);
     }
 
     render() {
         return (
             <div className="todo-header">
                 <TextArea id="content" ref={this.inputRef} cols={50} rows={7} />
-                {/* <textarea id="content" ref={this.inputRef} cols={50} rows={7}/> */}
+                {/* <textarea id="content" ref={this.inputRef} onChange={change2} cols={50} rows={7}/> */}
                 <br />
                 <br />
                 {/* <button onClick={this.sendToApp}>Post My Prediction</button> */}
