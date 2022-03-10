6a6af9310a657789ba0430ccc78d767fee1a3357
Yinbo <bohrplanet@gmail.com>
2022-03-09 18:31:51 -0800

    use testnet ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,27 @@
 const path = require("path");
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+// save private key at local, use secrets json to access.
+const mnemonic = require("./secrets.json").mnemonic
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    // develop: {
+    //   port: 8545
+    // },
+
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://speedy-nodes-nyc.moralis.io/c908d6e4fc7cc3c0f8b58460/eth/ropsten`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
   },
 
